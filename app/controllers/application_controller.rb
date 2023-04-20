ee9b0df085a8131c62f448fb90787e1525fe8095
leonardo-vinicius <lvwjs@ic.ufal.br>
2023-04-20 14:24:40 -0300

    alteracoes_heroku_autenticacao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  # before_action :authenticate_request
+  before_action :authenticate_request
   attr_reader :current_user
+
   private
 
   def authenticate_request
